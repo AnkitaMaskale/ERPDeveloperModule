e4450c95d16500fb71a31673c9ca1070f6435c5b
AnkitaMaskale <ankitamaskale4@gmail.com>
2025-03-27 15:23:21 +0530

    Add project files.

---
+++ ERPDeveloperModule/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ERPDeveloperModule.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
